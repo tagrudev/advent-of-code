5b593948550715fed9de8d2ca79e771f5b9d641b
Todor Grudev <tagrudev@gmail.com>
2021-12-13 09:23:26 +0200

    2021 - Day 13

---
+++ 2021/13/part1.kt
@@ -0,0 +1,34 @@
+import java.io.File
+
+fun lines(): List<String> {
+  return File("src/main/kotlin/input.txt").readText().trim().split("\n\n\n").first().lines()
+}
+
+class Point(var y: Int, var x: Int) {
+  override fun toString(): String = "($x, $y)"
+  fun getFromFold(fold: String): Int = if(fold.split("=").first() == "y") y else x
+  fun updateFromFold(direction: String, number: Int) { if (direction == "y") { y = number } else { x = number } }
+}
+
+val points = lines().takeWhile { it.isNotBlank() }.map { Point(it.split(",").last().toInt(), it.split(",").first().toInt()) }
+val folds = lines().takeLastWhile { it.isNotBlank() }.map { it.replace("fold along ", "") }
+
+fun main() {
+  println(foldPaper(folds.first()))
+}
+
+fun foldPaper(fold: String) : Int {
+  val max = points.maxOfOrNull { it.getFromFold(fold) }!!
+  val foldNumber = fold.split("=").last().toInt()
+  val foldDirection = fold.split("=").first()
+
+  points.filter { it.getFromFold(fold) > foldNumber }.groupBy { it.getFromFold(fold) }.forEach {
+    for((index, number) in (foldNumber+1..max).toList().withIndex()) {
+      if (it.key == number) {
+        it.value.forEach { point -> point.updateFromFold(foldDirection, 2 * foldNumber - (foldNumber+index+1)) }
+      }
+    }
+  }
+
+  return points.groupingBy { it.toString() }.eachCount().size
+}
