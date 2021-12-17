a03b4c0bd2a6b75acc658d3ab6bade4e550a51bc
Todor Grudev <tagrudev@gmail.com>
2021-12-17 11:13:03 +0200

    2021 - Day 17

---
+++ 2021/17/part1.kt
@@ -0,0 +1,56 @@
+import utils.challenge
+
+data class Point(var x: Int, var y: Int) {
+  fun within(gtx: Int, ltx: Int, bty: Int, lty: Int) : Boolean = this.x in gtx..ltx && this.y in bty..lty
+}
+
+fun main() {
+  challenge { lines ->
+    val ranges = lines.first().replace("target area: ", "").split(",").map { it.trim() }
+    var bruteForcePoints = mutableListOf<Point>()
+    for (i in -500 until 500) { for (j in -500 until 500) { bruteForcePoints.add(Point(j, i)) } }
+
+    val gtx = ranges.first().replace("x=", "").split("..").first().toInt()
+    val ltx = ranges.first().replace("x=", "").split("..").last().toInt()
+    val gty = ranges.last().replace("y=", "").split("..").first().toInt()
+    val lty = ranges.last().replace("y=", "").split("..").last().toInt()
+
+
+    var bruteForcedPoints = mutableListOf<Pair<Point, Int>>()
+    bruteForcePoints.forEach { bruteForcePoint ->
+      if (generateTrajectoryStepsFor(bruteForcePoint).take(500).any { it.within(gtx, ltx, gty, lty) }) {
+        var trajectoryPoints = generateTrajectoryStepsFor(bruteForcePoint)
+          .takeWhile { point -> !point.within(gtx, ltx, gty, lty) }
+          .toList()
+
+        if(trajectoryPoints.isEmpty()) return@forEach
+        bruteForcedPoints.add(bruteForcePoint to trajectoryPoints.maxOf { it.y })
+      }
+    }
+
+    println(bruteForcedPoints.maxOf { it.second })
+  }
+}
+
+fun generateTrajectoryStepsFor(point: Point) : Sequence<Point> {
+  var startX = point.x
+  var startY = point.y
+
+  var trajectorySteps = generateSequence(point) {
+    startY -= 1
+    if(startX > 0) {
+      startX -= 1
+    } else if (startX < 0) {
+      startX += 1
+    } else {
+      startX
+    }
+
+    var x = it.x + startX
+    var y = it.y + startY
+
+    Point(x, y)
+  }
+
+  return trajectorySteps
+}
